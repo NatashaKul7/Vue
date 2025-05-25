49f9b4d180c2d908b4757cc4b8921a1f8602168c
NatashaKul7 <natasha.kulibaba0703@gmail.com>
2025-05-25 22:01:11 +0200

    +

--- vue-course/02-how-works/app.js
+++ vue-course/02-how-works/app.js
@@ -11,7 +11,23 @@ const App = {
       items: [1, 2, 3, 4, 5, 6],
     };
   },
+  computed: {
+    evenItems() {
+      return this.items.filter((i) => i % 2 === 0);
+    },
+  },
   methods: {
+    addItem() {
+      this.items.unshift(this.$refs.myInput.value);
+      this.$refs.myInput.value = "";
+    },
+    remove(i) {
+      this.items.splice(i, 1);
+    },
+    log(item) {
+      console.log(item);
+    },
+
     // stopPropagation(event) {
     //   event.stopPropagation();
     // },
---
+++ vue-course/03-what-vue-have/app.js
@@ -0,0 +1,62 @@
+const h = Vue.h;
+
+const App = {
+  data() {
+    return {
+      title: "Это свойство template",
+    };
+  },
+  methods: {
+    changeTitle() {
+      this.title = "Изменили";
+    },
+  },
+  //   template: `
+  //     <div class="card center">
+  //       <h1>{{title}}</h1>
+  //       <button class="btn" @click="title = 'Изменили'">Change</button>
+  //     </div>
+  //   `,
+  render() {
+    return h(
+      "div",
+      {
+        class: "card center",
+      },
+      [
+        h("h1", {}, this.title),
+        h(
+          "button",
+          {
+            class: "btn",
+            onClick: this.changeTitle,
+          },
+          "Изменить"
+        ),
+      ]
+    );
+  },
+};
+
+const app = Vue.createApp(App);
+app.mount("#app");
+
+// ========
+
+let title = "Vue 3";
+let message = "Title this:" + title;
+// console.log(message);
+
+const data = {
+  title: "Vue 3",
+  message: "Title this: Vue 3",
+};
+
+const proxy = new Proxy(data, {
+  get(target, p) {
+    console.log(object);
+  },
+  set(target, key, value) {
+    console.log(object);
+  },
+});
