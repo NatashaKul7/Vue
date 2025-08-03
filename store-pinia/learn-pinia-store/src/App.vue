65dad63ff7d08fb283eecbb259bbd64ecb2ce9f6
NatashaKul7 <natasha.kulibaba0703@gmail.com>
2025-08-03 22:08:24 +0200

    counter with pinia

---
+++ store-pinia/learn-pinia-store/src/App.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <div class="app">
+    <nav>
+      <RouterLink to="/">Home</RouterLink> |
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+    <RouterView />
+  </div>
+</template>
+
+<style>
+.app {
+  text-align: center;
+}
+</style>
