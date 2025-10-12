f532ee54743592c14d30dc6681b5f0a7b1666987
NatashaKul7 <natasha.kulibaba0703@gmail.com>
2025-10-12 19:06:23 +0200

    vue-reusable

---
+++ vue-course/09-vue-reusability/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <app-alert
+      v-if="alert"
+      text="It is very important message"
+      title="Attention!"
+      type="danger"
+      closable
+      @close="alert = false"
+    ></app-alert>
+    <div class="card">
+      <button class="btn primary" @click="toggleAlert">
+        {{ alert ? "Hide" : "Show" }} messages
+      </button>
+    </div>
+
+    <app-block></app-block>
+  </div>
+</template>
+
+<script>
+import AppAlert from "./components/AppAlert.vue";
+import AppBlock from "./components/AppBlock.vue";
+
+export default {
+  data() {
+    return {
+      alert: false,
+    };
+  },
+  methods: {
+    toggleAlert() {
+      this.alert = !this.alert;
+    },
+  },
+  components: { AppAlert, AppBlock },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue-course/09-vue-reusability/src/components/AppBlock.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <app-alert
+      v-if="alert"
+      text="It is very important message"
+      title="Attention!"
+      type="primary"
+      @close="alert = false"
+    ></app-alert>
+
+    <div class="card">
+      <button class="btn primary" @click="toggleAlert">
+        {{ alert ? "Hide" : "Show" }} messages
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppAlert from "./AppAlert.vue";
+
+export default {
+  data() {
+    return {
+      alert: false,
+    };
+  },
+  methods: {
+    toggleAlert() {
+      this.alert = !this.alert;
+    },
+  },
+  components: { AppAlert },
+};
+</script>
+
+<style lang="scss" scoped></style>
