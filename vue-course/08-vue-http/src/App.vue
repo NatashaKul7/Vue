db9f5a01aff834b0deba39ba647c180b77c29c00
NatashaKul7 <natasha.kulibaba0703@gmail.com>
2025-10-12 18:16:29 +0200

    update

--- vue-course/08-vue-http/src/App.vue
+++ vue-course/08-vue-http/src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="container">
+    <AppAlert :alert="alert" @close="alert = null"></AppAlert>
+
     <form class="card" @submit.prevent="createPerson">
       <h2>Work with data base</h2>
 
@@ -12,16 +14,37 @@
         Create a person
       </button>
     </form>
+
+    <AppLoader v-if="loading"></AppLoader>
+
+    <AppPeopleList
+      v-else
+      :people="people"
+      @load="loadPeople"
+      @remove="removePerson"
+    ></AppPeopleList>
   </div>
 </template>
 
 <script>
+import AppPeopleList from "./AppPeopleList.vue";
+import AppAlert from "./AppAlert.vue";
+import AppLoader from "./AppLoader.vue";
+import axios from "axios";
+
 export default {
   data() {
     return {
       name: "",
+      people: [],
+      alert: null,
+      loading: false,
     };
   },
+  mounted() {
+    this.loadPeople();
+  },
+
   methods: {
     async createPerson() {
       // https://vue-with-http-57cbd-default-rtdb.europe-west1.firebasedatabase.app/people.json
@@ -35,17 +58,68 @@ export default {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-            firstNFame: this.name,
+            firstName: this.name,
           }),
           // eslint-disable-next-line prettier/prettier
         }
       );
 
       const firebaseData = await response.json;
-      console.log(firebaseData);
+
+      this.people.push({
+        firstName: this.name,
+        id: firebaseData.name,
+      });
       this.name = "";
     },
+
+    async loadPeople() {
+      try {
+        this.loading = true;
+        const { data } = await axios.get(
+          // eslint-disable-next-line prettier/prettier
+          "https://vue-with-http-57cbd-default-rtdb.europe-west1.firebasedatabase.app/people.json"
+        );
+
+        if (!data) {
+          throw new Error("The list of people is empty");
+        }
+
+        this.people = Object.keys(data).map((key) => {
+          return {
+            id: key,
+            firstName: data[key].firstName,
+          };
+        });
+        this.loading = false;
+      } catch (error) {
+        this.alert = {
+          type: "danger",
+          title: "Mistake",
+          text: error.message,
+        };
+        this.loading = false;
+      }
+    },
+
+    async removePerson(id) {
+      try {
+        const name = this.people.find((person) => person.id === id).firstName;
+
+        await axios.delete(
+          // eslint-disable-next-line prettier/prettier
+          `https://vue-with-http-57cbd-default-rtdb.europe-west1.firebasedatabase.app/people/${id}.json`
+        );
+        this.people = this.people.filter((person) => person.id !== id);
+        this.alert = {
+          type: "primary",
+          title: "Success",
+          text: `User ${name} was deleted`,
+        };
+      } catch (error) {}
+    },
   },
+  components: { AppPeopleList, AppAlert, AppLoader },
 };
 </script>
 
---
+++ vue-course/08-vue-http/src/AppPeopleList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="people.length !== 0">
+    <div class="card inline" v-for="person in people" :key="person.id">
+      <h3>{{ person.firstName }}</h3>
+      <button class="btn danger" @click="$emit('remove', person.id)">
+        Delete
+      </button>
+    </div>
+  </div>
+  <div v-else class="card center">
+    <h4>There're no people yet</h4>
+    <button class="btn" @click="$emit('load')">Load the list</button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["load", "remove"],
+  props: ["people"],
+};
+</script>
+
+<style lang="scss" scoped>
+.inline {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
